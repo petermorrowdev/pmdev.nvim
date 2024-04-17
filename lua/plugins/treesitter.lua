a1367a91c6c80a5fda035c62befe4ce3af196aa0
Peter Morrow <petermorrowtech@gmail.com>
2024-04-16 22:28:13 -0700

    Adds dockerfile

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'json', 'python', 'toml', 'sql' },
+    ensure_installed = { 'bash', 'c', 'dockerfile', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'json', 'python', 'toml', 'sql', 'hcl' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = { enable = true },
