fa8f2d1f60bb1392b8ce26295056d0dc639f94ec
Peter Morrow <petermorrowtech@gmail.com>
2025-05-28 17:08:30 -0700

    Use djlint for autoformatting `htmldjango` files

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,4 +1,4 @@
-return { -- Autoformat
+return {
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
@@ -13,12 +13,7 @@ return { -- Autoformat
       typescript = { 'prettierd' },
       javascriptreact = { 'prettierd' },
       typescriptreact = { 'prettierd' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      htmldjango = { 'djlint' },
     },
   },
 }
