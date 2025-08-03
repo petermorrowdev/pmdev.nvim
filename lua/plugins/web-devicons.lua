7994a496c5403702479e0dfee2e14510cd691caf
Peter Morrow <petermorrowtech@gmail.com>
2025-08-03 15:04:00 -0700

    Fix broken CSS icon

--- lua/plugins/web-devicons.lua
+++ lua/plugins/web-devicons.lua
@@ -3,5 +3,13 @@ return {
   enabled = vim.g.have_nerd_font,
   opts = {
     default = true,
+    override = {
+      css = {
+        -- CSS icon is broken, showing 歓 not sure why
+        icon = '',
+        color = '#3ca5d6',
+        name = 'Css',
+      },
+    },
   },
 }
