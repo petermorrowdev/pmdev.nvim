6ff647387f5dead63e107252e478ce829b8c90f0
Peter Morrow <petermorrowtech@gmail.com>
2025-11-20 00:17:50 -0800

    cut off long branch names with elipsis

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,26 @@
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
   opts = {
     options = {
       icons_enabled = vim.g.have_nerd_font,
       theme = 'greenight',
       component_separators = '',
       section_separators = '',
+    },
+    sections = {
+      lualine_b = {
+        {
+          'branch',
+          fmt = function(str)
+            if #str > 20 then
+              return str:sub(1, 32) .. '...'
+            end
+            return str
+          end,
+        },
+        'diff',
+        'diagnostics',
+      },
     },
   },
 }
